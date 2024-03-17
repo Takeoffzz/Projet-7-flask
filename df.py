53fe6472015448109ef798e0d9a74d3509c84b98
Sofiane <sofiane.deparis1@gmail.com>
2024-03-17 07:14:16 +0100

    update

--- df.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-df = pd.read_csv('submission_kernel02.csv')
-
-def recuperer_score_par_ID(id):
-        df2 = df[df['SK_ID_CURR'] == id]
-        list = df2['TARGET'].values.tolist()
-        return list
